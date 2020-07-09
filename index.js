7b657541ea0f217a5292c193eb515fbda67371b5
Luke Bradaric <lukebradaric@gmail.com>
2020-07-09 15:34:37 -0700

    Dynamic downloading
    Adding link builder to dynamically create clips from any game, any duration, and any limit. Added option to download all clips from JSON. Cleaned up all code.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const fetch = require('node-fetch')
-const leagueUrl = 'https://api.twitch.tv/kraken/clips/top?game=League+of+Legends&period=day&trending=true&limit=3'
-const tools = require('./tools')
+const fetch = require('node-fetch') //url requests?
+const download = require('download-file') //download file from url
+const tools = require('./tools'); //custom tools (conversion & link building)
 let dbg = false; //Debug mode
+let queue = []; //Queue of videos to download
 
 //Options for requesting twitch clips (Twitch API)
 const options = {
@@ -11,23 +12,78 @@ const options = {
     }
 }
 
-//Get top three clips to json (testing purposes)
-function getTopThree(url)
+//Download options for download-file
+const downloadOptions = {
+    directory: './clips/',
+    filename: 'UNNAMED.mp4'
+}
+
+//Get top clips from a url
+function getTopClips(url)
 {
     return fetch(url, options).then(res => res.json()).then(data => { return data })
 }
 
-//Get thumbnail of clip and grab mp4 from thumbnail
-let jn = getTopThree(leagueUrl)
-jn.then(result =>
+//Creates a queue of clips and DOWNLOADS THEM AUTOMATICALLY
+function createQueue(topClipsLink)
 {
-    if (dbg) console.log(result);
-
-    for (let x of result['clips'])
+    //Fetch json file from twitch api
+    let jn = getTopClips(topClipsLink)
+    //Create a queue of mp4 files from clips data
+    jn.then(result =>
     {
-        let imgLink = x.thumbnails['small']
-        if (dbg) console.log('Image Link: ' + imgLink)
-        let vidLink = tools.imgToVid(imgLink)
-        console.log('Video Link: ' + vidLink)
+        //Log the result of the html fetch
+        if (dbg) console.log(result);
+
+        //For each clip, create an mp4 link and add it to queue
+        for (let x of result['clips'])
+        {
+            //Convert clip thumbnail into video link and add to queue
+            let imgLink = x.thumbnails['small']
+            if (dbg) console.log('Image Link: ' + imgLink)
+            let vidLink = tools.imgToVid(imgLink)
+            queue.push(vidLink)
+        }
+
+        //call downloadQueue and download all clips in queue
+        if (!dbg) downloadQueue()
+
+        //Log the clips queue
+        if (dbg) console.log(queue)
+    })
+}
+
+//Download all files in the queue
+function downloadQueue()
+{
+    //Create link of last file to know when download queue is finished
+    let downloadCount = 0
+    let maxDownloadCount = queue.length
+    console.log('Downloading ' + maxDownloadCount + ' files in queue...')
+
+    //Loop through queue and download all files
+    for (let link of queue)
+    {
+        if (dbg) console.log('Downloading ' + link) //Log out file to download
+        let videoName = tools.vidLinkToName(link) //Create name for file
+        downloadOptions.filename = videoName; //Set the name of the next download
+        download(link, downloadOptions, function (err)
+        {
+            if (err) throw err
+            console.log('Successfully downloaded ' + videoName + ' at ' + link)
+
+            //Add one to amount of files downloaded
+            downloadCount++
+            //Check if all files were downloaded
+            if (downloadCount == maxDownloadCount)
+            {
+                //If finished downloading, clear queue and stich files
+                queue = []
+                console.log('All files downloaded. Ready to be stitched...');
+            }
+        })
     }
-})
+}
+
+//Build a link from tools (buildGameClipLink = top clips in a game category)
+createQueue(tools.buildGameClipLink('World of Warcraft', 'day', 3))
--- tools.js
+++ tools.js
@@ -1,4 +1,21 @@
 module.exports = {
+    //Build a twitch URL to download from (game clips)
+    buildGameClipLink: function (game, period, limit)
+    {
+        //game is string of game name, EX: 'League+of+Legends
+        //period is clip period, EX: 'day, week, month, all'
+        //limit is amount of clips to download, EX: '1,2,3,4,5,10,100'
+        let twLink = 'https://api.twitch.tv/kraken/clips/top?'
+        let gameLink = 'game=' + game + '&'
+        let periodLink = 'period=' + period + '&trending=true&language=en&'
+        let limitLink = 'limit=' + limit
+        return twLink + gameLink + periodLink + limitLink
+    },
+    //Build a twitch URL to download from (channel clips)
+    buildChannelClipLink: function (channel, period, limit)
+    {
+        //I haven't done this yet...
+    },
     //Convert thumbnail link to mp4 link
     imgToVid: function (link)
     {
@@ -6,5 +23,12 @@ module.exports = {
         let vidLink = link.substring(0, index)
         vidLink += '.mp4'
         return vidLink;
+    },
+    //Convert video link into a 6 digit name
+    vidLinkToName: function (link)
+    {
+        let index = link.indexOf('.mp4')
+        let name = link.substring(index - 6, link.length)
+        return name;
     }
 }
